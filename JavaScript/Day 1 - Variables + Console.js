5dfb0ec54611fedb80053b24b0ea1c8a06f66f9c
MagnoClarence <clarenceamagno@iskolarngbayan.pup.edu.ph>
2026-04-20 19:46:43 +0800

    Final Day 1 exercise

--- JavaScript/Day 1 - Variables + Console.js
+++ JavaScript/Day 1 - Variables + Console.js
@@ -1,11 +1,17 @@
 var userName = "John"; // This is a variable declaration and assignment. We are creating a variable named 'userName' and assigning it the value "John".
-const pi = 3.14; // This is a constant declaration and assignment. We are creating a constant named 'pi' and assigning it the value 3.14. Constants cannot be reassigned.
 let password = "password123"; // This is a variable declaration and assignment. We are creating a variable named 'password' and assigning it the value "password123".
+var isLoggedIn = false; // This is a variable declaration and assignment. We are creating a variable named 'isLoggedIn' and assigning it the value false.
+const pi = 3.14; // This is a constant declaration and assignment. We are creating a constant named 'pi' and assigning it the value 3.14. Constants cannot be reassigned.
 
+console.log("Value of userName before the if block: " + userName);  
 if (password === "password123") { // This is a conditional statement that checks if the value of 'password' is equal to "password123".
     let password = "newPassword456";
+    userName = "Jane"; // This is an assignment. We are changing the value of the 'userName' variable to "Jane".
+    isLoggedIn = true; // This is an assignment. We are changing the value of the 'isLoggedIn' variable to true.
+    console.log("Value of userName inside the if block: " + userName); 
     console.log("Value of password inside the if block: " + password); // This will log "Value of password inside the if block: newPassword456" to the console, as it is referring to the 'password' variable declared within the if block.
 }
-console.log("Username is: " + userName);     
+console.log("Is the user logged in? " + isLoggedIn); // This will log "Is the user logged in? true" to the console, as the value of 'isLoggedIn' was changed to true within the if block.
+console.log("Value of userName outside the if block: " + userName);     
 console.log("Value of password outside the if block: " + password); // This will log "password123" to the console, as it is referring to the 'password' variable declared outside the if block.     
-console.log(pi);
+console.log("Value of pi is: " + pi);
